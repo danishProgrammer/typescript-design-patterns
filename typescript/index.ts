56763eaa592882e196395fba0af553df6a199d03
Danish Arora <danisharora026@gmail.com>
2024-05-31 17:11:09 +0530

    encapsulation implementation

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,53 +1,52 @@
-// an example of Abstraction
+// Encapsulation ==> Data Hiding, Protection of concerns.
 
-interface Shape{
-    area():number,
-    perimeter():number
+//A demonstration of using encapsulation by using Bank Account example
+// Deposit Money
+// Withdrawal
+// Balance ==> encapsulated and hidden from end user.
+
+class BankAccount {
+    private _balance:number;
+
+    // Getter to get the balance of current account;
+    public get balance():number{
+        return this._balance;
+    }
+
+    constructor(initialBalance:number){
+        this._balance = initialBalance;
+    }
+    public deposit(amount:number):void{
+        if(amount < 0) {
+            console.log("Invalid deposit amount");
+            return;
+        }
+        this._balance += amount;
+    }
+    public withdrawal(amount:number):void{
+        if(amount < 0) {
+            console.log("Invalid Withdrawal amount");
+            return;
+        }
+        if((this._balance - amount) < 0) {
+            console.log("Insufficient funds");
+            return;
+        }
+        this._balance -= amount;
+    }
+
 }
 
-class Circle implements Shape{
-    constructor(private radius:number){
-        this.radius = radius;
-    }
-    area(): number {
-        return Math.PI * this.radius * this.radius;
-    }
-    perimeter(): number {
-        return 2 * Math.PI * this.radius;
-    }
-}
+const myBankAccount = new BankAccount(5000);
+myBankAccount.deposit(500);
+myBankAccount.withdrawal(300);
+console.log("Current Balance",myBankAccount.balance);
 
-class Rectangle implements Shape{
-    constructor(private length:number,private width:number){
-        this.length = length;
-        this.width = this.width;
-    }
-    area(): number {
-        return this.length * this.width;
-    }
-    perimeter(): number {
-        return 2 * (this.length + this.width);
-    }
-}
+/* Advantages of using Encapsulation.
 
-function calculateTotalArea(shape:Shape){
-    return shape.area();
-}
-
-const myCircle = new Circle(3);
-const myRectange = new Rectangle(4,6);
-// console.log(myCircle.area());
-// console.log(myCircle.perimeter());
-console.log('Area of circle',calculateTotalArea(myCircle));
-console.log('Area of rectangle',calculateTotalArea(myRectange));
-
-
-/* Advantages of using Abstraction
-
-Simplicity: Hidle Complex details.
-Maintainability: changes don't effect application.
-Reusability: Generic classes can be reused.
-Modularity:Each object manages its own behavior
-Security:  Hide internal state and implementation.
+Data Hiding: Prevent unauthorized access and misuse.
+Control Over Data: Provides control over data validation and integrity
+Flexibility & Extensibility
+Reusability & Maintainability
 
 */
