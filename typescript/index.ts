28aae6138f936166049458857cbcad215bd2a79a
Danish Arora <danisharora026@gmail.com>
2024-06-03 17:04:24 +0530

    polymorphisim understanding

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,52 +1,17 @@
-// Encapsulation ==> Data Hiding, Protection of concerns.
+// Polymorphism : Objects of different classes to be treated as objects of a common superclass.
 
-//A demonstration of using encapsulation by using Bank Account example
-// Deposit Money
-// Withdrawal
-// Balance ==> encapsulated and hidden from end user.
+/* Different Types of polymorphisim
+ 1) Subtype : aka inheritance or implementation polymorphism 
+ 2) Parametric : aka Generics
+ 3) Adhoc : aka function overloading or operator overloading
 
-class BankAccount {
-    private _balance:number;
+Realtime use case of Polymorphisim: Express Js
 
-    // Getter to get the balance of current account;
-    public get balance():number{
-        return this._balance;
-    }
+Implementation example can be used as same used in Abstraction.
 
-    constructor(initialBalance:number){
-        this._balance = initialBalance;
-    }
-    public deposit(amount:number):void{
-        if(amount < 0) {
-            console.log("Invalid deposit amount");
-            return;
-        }
-        this._balance += amount;
-    }
-    public withdrawal(amount:number):void{
-        if(amount < 0) {
-            console.log("Invalid Withdrawal amount");
-            return;
-        }
-        if((this._balance - amount) < 0) {
-            console.log("Insufficient funds");
-            return;
-        }
-        this._balance -= amount;
-    }
-
-}
-
-const myBankAccount = new BankAccount(5000);
-myBankAccount.deposit(500);
-myBankAccount.withdrawal(300);
-console.log("Current Balance",myBankAccount.balance);
-
-/* Advantages of using Encapsulation.
-
-Data Hiding: Prevent unauthorized access and misuse.
-Control Over Data: Provides control over data validation and integrity
-Flexibility & Extensibility
-Reusability & Maintainability
-
-*/
+Advantages of Polymorphisim
+ 1) Code Reusability.
+ 2) Interface consistency
+ 3) Flexibility & Scalability.
+ 4) Reduced complexity and Enhanced collaboration
+ */
