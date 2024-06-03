77e5dd18591bd712cd40b1f2bb5c882d88cddb40
Danish Arora <danisharora026@gmail.com>
2024-06-03 17:40:40 +0530

    inheritance in action

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,16 +1,64 @@
-class Animal{
-    constructor(public name:string){}
-
-    move(distance:number){
-        console.log(`${this.name} moved ${distance} meters`);
-    }
+class Product {
+  constructor(
+    public id: string,
+    public price: number,
+    public description: string
+  ) {}
+  display() {
+    console.log(`
+            ID: ${this.id},
+            Price: ${this.price},
+            Description: ${this.description}
+        `);
+  }
 }
 
-class Dog extends Animal{
-    constructor(public name:string = 'dog'){
-        super(name);
-    }
+class Book extends Product {
+  constructor(
+    public id: string,
+    public price: number,
+    public description: string,
+    public author: string,
+    public title: string
+  ) {
+    super(id,price,description);
+  }
+
+  display(): void {
+    super.display();
+      console.log(`
+      Author: ${this.author},
+      Title: ${this.title}
+      `)
+  }
 }
 
-const myDog = new Dog();
-myDog.move(10);
+class Electronic extends Product {
+    constructor(
+      public id: string,
+      public price: number,
+      public description: string,
+      public brand: string,
+      public model: string
+    ) {
+      super(id,price,description);
+    }
+  
+    display(): void {
+      super.display();
+        console.log(`
+        Brand: ${this.brand},
+        Model: ${this.model}
+        `)
+    }
+  }
+
+
+  const myBook = new Book("1",19.67,"A Good Book","John Dear","John's book");
+  myBook.display();
+
+  const laptop = new Electronic("2",90.67,"A Good Laptop","Dell","Dell's latitude");
+  laptop.display();
+
+
+  
